24c24992134276f3475f9d6c7015843f58453473
David <speckd@informatik.uni-freiburg.de>
2020-07-03 07:41:53 +0200

    clean up cudd

--- src/dd_libs/cudd-3.0.0/build.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-def main():
-    os.system("./configure --enable-obj --enable-dddmp \"CFLAGS=-m64 -Wall -Wextra -g -O3\" \"CXXFLAGS=-m64 -Wall -Wextra -g -std=c++14 -O3\" \"LDFLAGS=-m64\"")
-
-if __name__ == "__main__":
-    main()
